97b7fb8429905db0a52ac099418c5006fd54ca19
Wayne Moodie <wmoodie@cogswell.edu>
2018-07-31 00:40:44 -0700

    ui built needs to be connected to the function

--- MatrixConstraint.py
+++ MatrixConstraint.py
@@ -10,7 +10,7 @@
 
 from pymel.core import *
 from functools import wraps
-
+from MatrixConstraint.Qt import QtWidgets, QtCore, QtGui
 
 def undoFunc(func):
     @wraps(func)
@@ -45,8 +45,25 @@ def lockNull(object):
 @undoFunc
 def matrixConstraint(parent=False, point=False, orient=False, scale=False,
                      all=False, x=False, y=False, z=False , maintainOffset = False):
+    """
+    Creates a matrix based constraint network instead of Maya's default constraint system. Since we are using matrices
+    it runs a bit more efficient yielding .0375 fps increase per setup over Maya's constraint setup. results may vary
+    This was based on  Vasil Shotarov over at https://bindpose.com/maya-matrix-nodes-blending-matrices/.
+    Special thanks to him for sharing the information.
 
-
+    :param parent: Mark True for parent style constraint
+    :param point: Mark True for point style constraint
+    :param orient: Mark True for Orient style constraint
+    :param scale: Mark True for scale style constraint
+    :param all: Mark True all axes to be constrained
+    :param x: Mark True x axis to be constrained
+    :param y: Mark True y axis to be constrained
+    :param z: Mark True z axis to be constrained
+    :param maintainOffset: Mark True to MaintainOffeset
+    :param args: Provide selection of only transform nodes. They can be strings or PyNodes
+          I.E. matrixConstraint( 'cone1','cube1' parent=False, all =True)
+    :return: Constraint settings node
+    """
     my_list = ls(sl=True)
     select(d=True)
     # Error handling checks for selection and checks for a axes to constrain
@@ -139,6 +156,98 @@ def matrixConstraint(parent=False, point=False, orient=False, scale=False,
                 if axis[0]:
                     connectAttr(decomp + '.outputTranslate' + axis[-1], driven+'.translate'+axis[-1])
                     connectAttr(decomp + '.outputRotate' + axis[-1], driven + '.rotate' + axis[-1])
-if __name__ == "__main__":
 
-    matrixConstraint(orient=True, all=True, maintainOffset = True)
+#from PyQt5 import QtWidgets, QtCore, QtGui
+class MatrixConstraintUI(QtWidgets.QDialog):
+
+    def __init__(self):
+        super(MatrixConstraintUI, self).__init__()
+
+        self.setWindowTitle('Matrix Constraints')
+
+        self.buildUI()
+        self.populateUI()
+
+    def buildUI(self):
+        gridAx = QtWidgets.QGridLayout(self)
+
+
+        maintainOffsetLabel = QtWidgets.QLabel('Maintain Offset:')
+        maintainOffsetLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        maintainOffsetBtn = QtWidgets.QCheckBox()
+        gridAx.addWidget(maintainOffsetLabel, 0, 0)
+        gridAx.addWidget(maintainOffsetBtn, 0, 1)
+
+
+
+
+        matrixLabel = QtWidgets.QLabel('Constraint Axes:')
+        matrixLabel.setAlignment(QtCore.Qt.AlignRight| QtCore.Qt.AlignVCenter)
+        gridAx.addWidget(matrixLabel, 1, 0)
+
+
+        pointLabel     = QtWidgets.QLabel('Point Constraint:')
+        pointLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        pointConBtn    = QtWidgets.QCheckBox('All')
+        gridAx.addWidget(pointLabel, 2, 0)
+        gridAx.addWidget(pointConBtn, 2, 1)
+
+        pointX,pointY,pointZ = QtWidgets.QCheckBox('X'),QtWidgets.QCheckBox('Y'),QtWidgets.QCheckBox('Z')
+        gridAx.addWidget(pointX, 3, 1)
+        gridAx.addWidget(pointY, 3, 2)
+        gridAx.addWidget(pointZ, 3, 3)
+
+
+
+        parentLabel    = QtWidgets.QLabel('Parent Constraint:')
+        parentLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        parentConBtn   = QtWidgets.QCheckBox('All')
+        gridAx.addWidget(parentLabel, 4, 0)
+        gridAx.addWidget(parentConBtn, 4, 1)
+
+        parentX, parentY, parentZ = QtWidgets.QCheckBox('X'), QtWidgets.QCheckBox('Y'), QtWidgets.QCheckBox('Z')
+        gridAx.addWidget(parentX, 5, 1)
+        gridAx.addWidget(parentY, 5, 2)
+        gridAx.addWidget(parentZ, 5, 3)
+
+        orientLabel    = QtWidgets.QLabel('Orient Constraint:')
+        orientLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        orientConBtn   = QtWidgets.QCheckBox('All')
+        gridAx.addWidget(orientLabel, 6, 0)
+        gridAx.addWidget(orientConBtn, 6, 1)
+
+        orientX, orientY, orientZ = QtWidgets.QCheckBox('X'), QtWidgets.QCheckBox('Y'), QtWidgets.QCheckBox('Z')
+        gridAx.addWidget(orientX, 7, 1)
+        gridAx.addWidget(orientY, 7, 2)
+        gridAx.addWidget(orientZ, 7, 3)
+
+        scaleLabel     = QtWidgets.QLabel('Scale Constraint:')
+        scaleLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        scaleConBtn    = QtWidgets.QCheckBox('All')
+        gridAx.addWidget(scaleLabel, 8, 0)
+        gridAx.addWidget(scaleConBtn, 8, 1)
+
+        scaleX, scaleY, scaleZ = QtWidgets.QCheckBox('X'), QtWidgets.QCheckBox('Y'), QtWidgets.QCheckBox('Z')
+        gridAx.addWidget(scaleX, 9, 1)
+        gridAx.addWidget(scaleY, 9, 2)
+        gridAx.addWidget(scaleZ, 9, 3)
+
+
+
+    def populateUI(self):
+        print
+        'populating it '
+
+
+def showUI():
+    ui = MatrixConstraintUI()
+    ui.show()
+    return ui
+
+
+
+
+
+if __name__ == "__main__":
+    ui = showUI()
+    #matrixConstraint(parent=True, all=True)
